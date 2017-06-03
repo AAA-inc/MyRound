6d3ab348172c860992be32bb23e2aafdf96aefc6
Anita <a.nita@hotmail.co.uk>
2017-06-03 23:08:41 +0100

    adds slides to landing page, removes Signup file

--- src/views/auth/SignIn.js
+++ src/views/auth/SignIn.js
@@ -1,21 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, Text, Button } from 'react-native';
+import { View, Button, Dimensions } from 'react-native';
 import { onSignIn } from './auth';
+import Slides from './Slides';
+
+const height = Dimensions.get('window').height;
+
+const SLIDE_DATA = [
+    { text: 'Welcome to My Round', color: 'pink' },
+    { text: 'Strapline Text 2', color: '#009688' },
+    { text: 'Strapline Text 3', color: '#03A9F4' }
+];
+
+const styles = {
+    container: {
+        flex: 1,
+        backgroundColor: 'transparent'
+    },
+    buttonsWrapper: {
+        flexDirection: 'row'
+    }
+};
 
 export default function SignIn ({ navigation }) {
     return (
-        <View>
-            <Text style={{ marginTop: 50 }}>Welcome to MyRound</Text>
-            <Button
-                buttonStyle={{ marginTop: 20 }}
-                backgroundColor='transparent'
-                textStyle={{ color: '#bcbec1' }}
-                title='FACEBOOK SIGN IN BUTTON'
-                onPress={() => {
-                    onSignIn().then(() => navigation.navigate('Authenticated'));
-                }}
-            />
+        <View style={styles.container}>
+            <View style={{ height }}>
+                <Slides data={SLIDE_DATA} onComplete={this.onSlidesComplete} />
+                <Button
+                    buttonStyle={{ marginTop: 20, width: 200 }}
+                    backgroundColor='blue'
+                    textStyle={{ color: '#fff' }}
+                    title='FACEBOOK'
+                    onPress={() => {
+                        onSignIn().then(() => navigation.navigate('Authenticated'));
+                    }}
+                />
+            </View>
         </View>
     );
 }
